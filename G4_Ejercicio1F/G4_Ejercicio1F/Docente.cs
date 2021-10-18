ee9875ee2e65f92b66fc5c15759f1cc01ec0068f
luispablovelasco <pablo.velascoflores@gmail.com>
2021-10-17 21:53:54 -0600

    Creación de la clase abstracta Persona en la que heredan la clase Estudiante y la clase Docentes

---
+++ G4_Ejercicio1F/G4_Ejercicio1F/Docente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4_Ejercicio1F
+{
+    class Docente : Persona
+    {
+
+        string materia;
+
+        public string Materia
+        {
+            get { return materia; }
+            set { materia = value; }
+        }
+
+    }
+}
---
+++ G4_Ejercicio1F/G4_Ejercicio1F/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4_Ejercicio1F
+{
+    abstract class Persona
+    {
+
+        string nombre;
+        string usuario;
+        string codigo;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+    }
+}
